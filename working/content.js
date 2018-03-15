d9a3201376d70486d451b8d627c738511e05e46f
yinjisheng <jisheng_yin@mymail.sutd.edu.sg>
2018-03-15 14:17:14 +0800

    removal of angular codes

--- working/content.js
+++
@@ -1,8 +0,0 @@
-// var div=document.createElement("div"); 
-// document.body.insertBefore("body")
-// div.innerText="test123";
-
-var iFrame  = document.createElement ("iframe");
-iFrame.src  = chrome.extension.getURL ("index.html");
-
-document.body.insertBefore (iFrame, document.body.firstChild);
